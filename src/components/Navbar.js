80956e1f35f1784db25da9c3cae8f86f3632affb
Rajneeshkumar12345 <rkv7861999@gmail.com>
2022-05-13 20:50:10 +0530

    new

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -28,11 +28,7 @@ export default function Navbar(props) {
         </form> */}
         
             <div className={`form-check form-switch text-${props.mode==='light' ? 'Dark' : 'light'}`}>
-      <input className="form-check-input" onClick={()=>{
-
-        props.toggleMode()
-
-      }} type="checkbox" role="switch" id="flexSwitchCheckDefault"/>
+            <input className="form-check-input" onClick={props.toggleMode} type="checkbox" id="flexSwitchCheckDefault"/>
       <label className="form-check-label" htmlFor="flexSwitchCheckDefault">Enable Dark Mode</label>
     </div>
       </div>
--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -44,13 +44,13 @@
       <div className="container" style={{color: props.mode==='dark'?'white':'#042743'}}>
       <h1> {props.headings}</h1>
       <div className="mb-3">
-      <textarea className="form-control"  value={text} onChange={handleOnChange} style={{backgroundColor: props.mode==='dark'?'grey':'white', color: props.mode==='dark'?'white':'#042743'}}  id="mybox" rows="5"></textarea>
+      <textarea className="form-control"  value={text} onChange={handleOnChange} style={{backgroundColor: props.mode==='dark'?'#13466e':'white', color: props.mode==='dark'?'white':'#042743'}}  id="mybox" rows="5"></textarea>
       </div>
-    <button className="btn btn-primary mx-1 my-1" onClick={handleUpClick}>Convert to UpperCase</button>
-    <button className="btn btn-primary mx-1 my-1" onClick={handleLoClick}>Convert to LowerCase</button>
-    <button className="btn btn-primary mx-1 my-1" onClick={handleCopy}>Copy Text</button>
-    <button className="btn btn-primary mx-1 my-1" onClick={handleClearClick}>Clear Text</button>
-    <button className="btn btn-primary mx-1 my-1" onClick={handleExtraSpaces}>Handle Extra Spaces</button>
+    <button disabled={text.length===0} className="btn btn-primary mx-1 my-1" onClick={handleUpClick}>Convert to UpperCase</button>
+    <button disabled={text.length===0} className="btn btn-primary mx-1 my-1" onClick={handleLoClick}>Convert to LowerCase</button>
+    <button disabled={text.length===0} className="btn btn-primary mx-1 my-1" onClick={handleCopy}>Copy Text</button>
+    <button disabled={text.length===0} className="btn btn-primary mx-1 my-1" onClick={handleClearClick}>Clear Text</button>
+    <button disabled={text.length===0} className="btn btn-primary mx-1 my-1" onClick={handleExtraSpaces}>Handle Extra Spaces</button>
     </div>
 
     <div className="container my-3" style={{color: props.mode==='dark'?'white':'#042743'}}>
@@ -58,7 +58,7 @@
         <p>{text.split(" ").filter((element) =>{return element.length !== 0}).length} Words and {text.length} Characters</p>
         <p>{0.008 * text.split("").filter((element) =>{return element.length !== 0}).length} Minutes read</p>
         <h2>Preview</h2>
-        <p>{text}</p>
+            <p>{text.length>0?text:"Nothing to preview!"}</p>
     </div>
     </>
       
